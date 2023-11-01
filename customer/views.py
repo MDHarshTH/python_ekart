2ad11322d23d62f6c1aea2b1a443da8d4a9fd0bb
MDHarshTH <mdhannath8@gmail.com>
2023-11-01 11:52:13 +0530

    change password

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from random import randint
+from django.conf import settings
+from django.core.mail import send_mail
 
-from customer.models import Customer
+from customer.models import Customer, Seller
 # from .models import Customer
 # Create your views here.
 
@@ -34,15 +37,71 @@ def dashboard(request):
 
 
 def seller_register(request):
-    return render(request, 'customer/seller_register.html')
+    status = False
+    msg = ''
+    if request.method == 'POST':
+        f_name = request.POST['fname']
+        l_name = request.POST['lname']
+        email = request.POST['email']
+        gender = request.POST['gender']
+        city = request.POST['city']
+        country = request.POST['country']
+        cmp_name = request.POST['cmpname']
+        bank_name = request.POST['bname']
+        bank_branch = request.POST['bbranch']
+        accno = request.POST['accnum']
+        ifsc = request.POST['ifsccode']
+        seller_image = request.FILES['uploadimage']
+
+        seller_exist = Seller.objects.filter(email = email).exists()
+        if not seller_exist : 
+            seller = Seller ( 
+                first_name = f_name,
+                last_name = l_name,
+                email = email,
+                gender = gender,
+                city = city,
+                country = country,
+                company_name = cmp_name,
+                seller_picture = seller_image,
+                account_number = accno,
+                ifsc = ifsc,
+                bank_name = bank_name, 
+                bank_branch =  bank_branch
+            )
+
+            seller.save()
+            msg = 'ACCOUNT CREATED SUCCESSFULLY'
+            status = True
+        else :
+            msg = 'EMAIL ALREADY EXIST'
+
+    return render(request, 'customer/seller_register.html',{'message' : msg})
 
 
 def seller_login(request):
-    return render(request, 'customer/seller_login.html')
 
+    msg = ''
+    if request.method == 'POST':
+        S_Id = request.POST['sellerid']
+        S_password = request.POST['sellerpass']
+
+        newSeller = Seller.objects.filter(login_id = S_Id,password = S_password)
+
+        if newSeller.exists():
+            request.session['seller'] = newSeller[0].id
+            request.session['seller_name'] = newSeller[0].first_name + ' ' + newSeller[0].last_name
+            return redirect('Seller:seller_home')
+        # msg = 'SUCCESSFULLY LOGIN'
+        else:
+            msg = 'Incorrect Password or Username'
+            
+
+    return render(request, 'customer/seller_login.html',{'message':msg})
 
 def customer_signup(request):
 
+    status = False
     msg = ""
 
     if request.method == 'POST':
@@ -54,7 +113,10 @@ def customer_signup(request):
         country = request.POST['country']
         password = request.POST['password']
 
-        customer = Customer(
+        customer_exist = Customer.objects.filter(email = email).exists()
+        
+        if not customer_exist: 
+            customer = Customer(
             first_name = f_name,
             last_name = l_name,
             email = email,
@@ -62,16 +124,30 @@ def customer_signup(request):
             city = city,
             country = country,
             password = password
-        )
-
-        customer.save()
-        msg = 'REGISTERED SUCCESSFULL'
-
-    return render(request, 'customer/customer_signup.html', {'message': msg})
+            )
+            customer.save()
+            msg = 'REGISTERED SUCCESSFULL'
+            status = True
+        else:
+            msg = 'ALREADY REGISTERED'
+    return render(request, 'customer/customer_signup.html', {'message': msg,'status':status})
 
 
 def customer_login(request):
-    return render(request, 'customer/customer_login.html')
+    msg = ''
+    if request.method == 'POST':
+        c_email = request.POST['email']
+        c_password = request.POST['password']
+
+        customer = Customer.objects.filter(email = c_email,password = c_password)
+
+        if customer.exists():
+            request.session['customer'] = customer[0].id
+            return redirect('customer:customer_home')
+        # msg = 'SUCCESSFULLY LOGIN'
+    else:
+        msg = 'Incorrect Password or Username'
+    return render(request, 'customer/customer_login.html',{'message':msg})
 
 
 def forgot_password_customer(request):
--- eKart_admin/views.py
+++ eKart_admin/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from random import randint
+from django.conf import settings
+from django.shortcuts import redirect, render
+from django.core.mail import send_mail
+
+from customer.models import Seller
 
 # Create your views here.
 def admin_home(request):
@@ -10,11 +15,33 @@ def view_category(request):
 def add_category(request):
     return render(request,'ekart_admin/add_category.html')
 
+def approve_seller(request, id):
+    seller = Seller.objects.get(id = id)
+    seller_id = randint(111111,999999)
+    temporary_password = 'sel-' + str(seller_id)
+    subject = 'Login credentials'
+    message = 'Hai! Your Ekart Account Has Been Approved. Your Id Is ' + str(seller_id) + 'And Password Is' + temporary_password
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [seller.email]
+    send_mail(
+        subject,
+        message,
+        from_email,
+        recipient_list
+    )
+    Seller.objects.filter(id = id).update(login_id = seller_id, password = temporary_password, status = 'Approved')
+    return redirect('ekart_admin:pending_sellers')
+
 def pending_sellers(request):
-    return render(request,'ekart_admin/pending_sellers.html')
+    pending_list = Seller.objects.filter(status = 'pending',)
+    return render(request,'ekart_admin/pending_sellers.html',{'list': pending_list})
 
 def approved_sellers(request):
     return render(request,'ekart_admin/approved_sellers.html')
 
 def customers(request):
     return render(request,'ekart_admin/customers.html')
+
+def admin_login(request):
+    return render(request,'ekart_admin/admin_login.html')
+
